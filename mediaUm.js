44ed535a4c15d502400ce8ad83f15fdbe9a30621
dany <danybatista046@gmail.com>
2021-02-22 17:43:25 -0300

    Media Um

---
+++ mediaUm.js
@@ -0,0 +1,28 @@
+// Forma que fiz para exibir o resultado no terminal
+var notaA = 5.0;
+var notaB = 7.1;
+
+// var A = 0.0;
+// var B = 7.1;
+
+// var A = 10.0;
+// var B = 10.0;
+
+var MEDIA;
+PROD = (notaA * notaB);
+
+MEDIA = ((notaA * 3.5) + (notaB * 7.5))/11;
+console.log("MÉDIA = " + MEDIA.toFixed(5));
+
+
+// Forma que fiz para exibir em um editor online e no console do navegador
+// var notaA;
+// var notaB;
+// var MEDIA;
+
+// notaA = parseFloat(prompt("Digite a primeira nota: "));
+// notaB = parseFloat(prompt("Digite a segunda nota: "));
+
+// MEDIA = ((notaA * 3.5) + (notaB * 7.5))/11;
+
+// console.log("MÉDIA = " + MEDIA.toFixed(5));
